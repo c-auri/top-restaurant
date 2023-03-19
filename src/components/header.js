641ecf8a9a39e66c2a05f63add92a8e0ebf5ae7b
c-auri <43008483+c-auri@users.noreply.github.com>
2023-03-19 18:45:18 +0100

    Implement nav

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,12 @@
+import { getNav } from "./nav"
+
 export function getHeader() {
     const header = document.createElement('header')
     const heading = document.createElement('h1')
     heading.textContent = 'Tea House TOP'
 
     header.appendChild(heading)
+    header.appendChild(getNav())
 
     return header
 }
