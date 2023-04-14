b0669d373a09e091278dffd1098f21a2ce55fcc4
c-auri <43008483+c-auri@users.noreply.github.com>
2023-04-14 19:18:23 +0200

    Set up image loading

--- webpack.common.js
+++ webpack.common.js
@@ -16,7 +16,13 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
-      }
+      },
+      {
+
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+
+      },
     ],
   },
   plugins: [
